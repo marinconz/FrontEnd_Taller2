562ca957393f946031e3216b9224c5d1b0d0f109
Maria Antonia Rincon <marinconz@eafit.edu.co>
2020-05-13 18:29:53 -0500

    Taller 3-Persistencia

--- temphum/views.py
+++ temphum/views.py
@@ -4,12 +4,15 @@ import requests
 # Create your views here.
 def temphum(request):
     # Verifica si hay un parámetro value en la petición GET
-    if 'value' in request.GET:
+    if 'value' and 'longitud' and 'latitud' and 'tipo_Terreno' in request.GET:
         value = request.GET['value']
+        longitud = request.GET['longitud']
+        latitud = request.GET['latitud']
+        tipo_Terreno = request.GET['tipo_Terreno']
         # Verifica si el value no esta vacio
         if value:
             # Crea el json para realizar la petición POST al Web Service
-            args = {'type': 'agua', 'value': value}
+            args = {'type': 'agua', 'value': value, 'longitud':longitud, 'latitud':latitud, 'tipo_Terreno':tipo_Terreno}
             response = requests.post('http://127.0.0.1:8000/temphum/', args)
             # Convierte la respuesta en JSON
             measure_json = response.json()
@@ -18,5 +21,7 @@ def temphum(request):
     response = requests.get('http://127.0.0.1:8000/temphum/')
     # Convierte la respuesta en JSON
     temphum = response.json()
+    
     # Rederiza la respuesta en el template measure
     return render(request, "temphum/temphum.html", {'temphums': temphum})
+    
